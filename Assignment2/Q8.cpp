723a4f24dcbfdb45c7aa2134c8d4b971a535ff80
Anshika Saxena <asaxena_be24@thapar.edu>
2025-08-26 14:34:39 +0530

    Update Q8.cpp

--- Assignment2/Q8.cpp
+++ Assignment2/Q8.cpp
@@ -8,7 +8,8 @@ int main() {
 
     int arr[n];
     cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) cin >> arr[i];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
     int count = 0;
     for (int i = 0; i < n; i++) {
